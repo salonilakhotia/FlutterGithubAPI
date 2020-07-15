7557586b188e5c75ae448ce8537d19e7ca5d1cf4
salonilakhotia <salonilakhotia242@gmail.com>
2020-07-16 02:15:26 +0530

    search screen

---
+++ android/app/src/main/kotlin/com/example/githubAPI/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.githubAPI
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new HomePage(),
+      debugShowCheckedModeBanner: false,
+     // theme : new ThemeData(primarySwatch : Colors.black,),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false, 
+      body: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80, ),
+          child: Column(children: <Widget>[
+            SizedBox(height:80,),
+            Container(
+              width: 130,
+              height: 130,
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                backgroundImage: NetworkImage('https://icon-library.net/images/github-icon-png/github-icon-png-29.jpg'),
+              ),
+            ),
+            SizedBox(height: 40,),
+            Text("Github", 
+            style : TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 45,
+            )),
+            SizedBox(height:40,),
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white.withOpacity(.5),
+              ),
+              child: TextField(
+                style: TextStyle(color: Colors.white,),
+                decoration: InputDecoration(
+                  hintText: "Github Username",
+                  hintStyle: TextStyle(color: Colors.grey,),
+                  border: InputBorder.none,
+                ),
+              ),
+             
+            ),
+             SizedBox(height: 20,),
+             MaterialButton(
+               height: 50.0,
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius:BorderRadius.circular(10),
+                ),
+                onPressed: () {  },
+                child: Center(child: Text("Search About User" , style:TextStyle(color: Colors.black,),),
+              ),
+             ),
+          ],),
+        ),
+      ),
+      
+    );
+  }
+}
